e115514097aaf8a2b91b5e38a0506aa41c190833
Mikael <mikael.snellman@cs.helsinki.fi>
2016-01-08 14:26:20 +0200

    refactor

--- src/main/java/com/mycompany/hackernewsuutiset/HTTPClient.java
+++ src/main/java/com/mycompany/hackernewsuutiset/HTTPClient.java
@@ -2,6 +2,7 @@
 package com.mycompany.hackernewsuutiset;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -18,22 +19,27 @@ public class HTTPClient {
 			urlConn = url.openConnection();
 			if (urlConn != null)
 				urlConn.setReadTimeout(60 * 1000);
-			if (urlConn != null && urlConn.getInputStream() != null) {
-				in = new InputStreamReader(urlConn.getInputStream(),
-						Charset.defaultCharset());
-				BufferedReader bufferedReader = new BufferedReader(in);
-				if (bufferedReader != null) {
-					int cp;
-					while ((cp = bufferedReader.read()) != -1) {
-						sb.append((char) cp);
-					}
-					bufferedReader.close();
-				}
-			}
+                in = checkConn(urlConn, in, sb);
 		in.close();
 		} catch (Exception e) {
 			throw new RuntimeException("Virhe hakiessa osoitteesta: "+ URLString, e);
 		} 
 		return sb.toString();
 	}
+
+    private static InputStreamReader checkConn(URLConnection urlConn, InputStreamReader in, StringBuilder sb) throws IOException {
+        if (urlConn != null && urlConn.getInputStream() != null) {
+            in = new InputStreamReader(urlConn.getInputStream(),
+                    Charset.defaultCharset());
+            BufferedReader bufferedReader = new BufferedReader(in);
+            if (bufferedReader != null) {
+                int cp;
+                while ((cp = bufferedReader.read()) != -1) {
+                    sb.append((char) cp);
+                }
+                bufferedReader.close();
+            }
+        }
+        return in;
+    }
 }
