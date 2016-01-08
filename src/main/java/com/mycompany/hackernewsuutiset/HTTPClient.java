b2dc08d8f0e898da65bcfcd4f45109b8abe5292a
Mikael <mikael.snellman@cs.helsinki.fi>
2016-01-08 14:26:54 +0200

    remove all

--- src/main/java/com/mycompany/hackernewsuutiset/HTTPClient.java
+++ src/main/java/com/mycompany/hackernewsuutiset/HTTPClient.java
@@ -2,7 +2,6 @@
 package com.mycompany.hackernewsuutiset;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -11,38 +10,30 @@ import java.nio.charset.Charset;
 
 public class HTTPClient {
     public static String callURL(String URLString) {
-                System.out.println("");
 		StringBuilder sb = new StringBuilder();
 		URLConnection urlConn = null;
-                System.out.println("");
 		InputStreamReader in = null;
 		try {
 			URL url = new URL(URLString);
 			urlConn = url.openConnection();
 			if (urlConn != null)
 				urlConn.setReadTimeout(60 * 1000);
-                in = checkConn(urlConn, in, sb);
+			if (urlConn != null && urlConn.getInputStream() != null) {
+				in = new InputStreamReader(urlConn.getInputStream(),
+						Charset.defaultCharset());
+				BufferedReader bufferedReader = new BufferedReader(in);
+				if (bufferedReader != null) {
+					int cp;
+					while ((cp = bufferedReader.read()) != -1) {
+						sb.append((char) cp);
+					}
+					bufferedReader.close();
+				}
+			}
 		in.close();
-                System.out.println("");
 		} catch (Exception e) {
 			throw new RuntimeException("Virhe hakiessa osoitteesta: "+ URLString, e);
 		} 
 		return sb.toString();
 	}
-
-    private static InputStreamReader checkConn(URLConnection urlConn, InputStreamReader in, StringBuilder sb) throws IOException {
-        if (urlConn != null && urlConn.getInputStream() != null) {
-            in = new InputStreamReader(urlConn.getInputStream(),
-                    Charset.defaultCharset());
-            BufferedReader bufferedReader = new BufferedReader(in);
-            if (bufferedReader != null) {
-                int cp;
-                while ((cp = bufferedReader.read()) != -1) {
-                    sb.append((char) cp);
-                }
-                bufferedReader.close();
-            }
-        }
-        return in;
-    }
 }
